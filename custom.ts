689743f4298fdcc65ba84d85fdb7e023f60c3125
Glen <glen.swindell@gtsoft.co.uk>
2017-03-31 22:56:57 +0100

    Made into blinky

--- custom.ts
+++ custom.ts
@@ -6,21 +6,41 @@
 
 
 /**
- * Custom blocks
+ * GTSoft custom functions
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace gtsoft.test {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Prints a string character at a time
+     * @param s the string to print
+     * @param w the time between characters in milliseconds
      */
     //% block
-    export function printString(s: string): void {
+    export function printString(s: string, w: uint16): void {
         for (let i = 0; i < s.length; i++) {
             basic.showString(s[i])
-            basic.pause(300)
+            basic.pause(w)
         }
     }
+
+    /**
+     * turns on a digital pin for a number of milliseconds
+     * @param w the time to turn on the pin
+     */
+    //% block
+    export function turnOn(w: uint16): void {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(w)
+    }
+
+     /**
+     * turns off a digital pin for a number of milliseconds
+     * @param w the time to turn on the pin
+     */
+    //% block
+    export function turnOff(w: uint16): void {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(w)
+    }
+   
 }
--- main.ts
+++ main.ts
@@ -1,17 +1,9 @@
-input.onButtonPressed(Button.A, () => {
-    basic.showString("Button Pressed!!")
-    basic.pause(500)
-})
-
-input.onButtonPressed(Button.B, () => {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-})
 
 basic.forever(() => {
-    gtsoft.test.printString("Hello JavaScript!!!")
-    basic.pause(500)
-    gtsoft.test.printString("Hello Glen!!!")
-    basic.pause(500)
+    // gtsoft.test.printString("Hello JavaScript!!!", 300)
+    // basic.pause(500)
+    // gtsoft.test.printString("Hello Glen!!!", 300)
+    // basic.pause(500)
+    gtsoft.test.turnOn(1000)
+    gtsoft.test.turnOff(1000)
 })
