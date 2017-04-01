aa822412d50f09be6efe2ae7ef802c1612946941
Glen <glen.swindell@gtsoft.co.uk>
2017-04-01 13:24:00 +0100

    Final version with comments

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
+// define our variable
 let isOn = 0
 
+// Make sure the pin is off to start with
+pins.digitalWritePin(DigitalPin.P0, 0)
+
+// set up the button press event
 input.onButtonPressed(Button.B, () => {
     if (isOn == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         isOn = 1
     } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
         isOn = 0
     }
+    
+    pins.digitalWritePin(DigitalPin.P0, isOn)
 })
 
-basic.forever(() => {
-    // gtsoft.test.printString("Hello JavaScript!!!", 300)
-    // basic.pause(500)
-    // gtsoft.test.printString("Hello Glen!!!", 300)
-    // basic.pause(500)
-    // gtsoft.test.turnOn(250)
-    // gtsoft.test.turnOff(250)
-})
+
+
+// basic.forever(() => {
+//     gtsoft.test.printString("Hello JavaScript!!!", 300)
+//     basic.pause(500)
+//     gtsoft.test.printString("Hello Glen!!!", 300)
+//     basic.pause(500)
+//     gtsoft.test.turnOn(250)
+//     gtsoft.test.turnOff(250)
+// })
