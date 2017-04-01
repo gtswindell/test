8f0efd74b9d9bb8c116083be108e93f59d452fb1
Glen <glen.swindell@gtsoft.co.uk>
2017-04-01 11:30:47 +0100

    Few changes to operate led via buttons

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
+let isOn = 0
+
+input.onButtonPressed(Button.B, () => {
+    if (isOn == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        isOn = 1
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        isOn = 0
+    }
+})
 
 basic.forever(() => {
     // gtsoft.test.printString("Hello JavaScript!!!", 300)
     // basic.pause(500)
     // gtsoft.test.printString("Hello Glen!!!", 300)
     // basic.pause(500)
-    gtsoft.test.turnOn(250)
-    gtsoft.test.turnOff(250)
+    // gtsoft.test.turnOn(250)
+    // gtsoft.test.turnOff(250)
 })
